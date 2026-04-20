d64ed516a807a454fc39061ce97d54e911626627
Shweta Rajput <shwetarajput0806@gmail.com>
2026-04-20 18:46:56 +0530

    Add files via upload

---
+++ Array/array4.cpp
@@ -0,0 +1,25 @@
+//Reverse an array
+#include<iostream>
+using namespace std;
+int Reverse(int arr[],int size){
+    int start =0;
+    int end = size-1;
+    while(start<end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+int main(){
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int size = 10;
+
+    Reverse(arr, size);
+
+    cout << "The reverse of array is: ";
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
