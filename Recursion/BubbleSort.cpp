5658f925a40e1eaca3f397320c8c6702e867bdf3
Shweta Rajput <shwetarajput0806@gmail.com>
2026-04-24 12:31:31 +0530

    Implement recursive bubble sort in BubbleSort.cpp

---
+++ Recursion/BubbleSort.cpp
@@ -0,0 +1,26 @@
+//Bubble sort using recursion
+
+#include<iostream>
+using namespace std;        
+void bubbleSort(int arr[],int n){
+    //base case
+    if(n==0 || n==1)
+        return;
+    for(int i=0;i<n-1;i++){
+        if(arr[i]>arr[i+1])
+            swap(arr[i],arr[i+1]);
+    }
+    //recursive call
+    bubbleSort(arr,n-1);
+}
+int main(){
+    int arr[5] = {5, 4, 3, 2, 1};
+    int size = 5;
+    bubbleSort(arr,size);
+    cout<<"Sorted array: ";
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
