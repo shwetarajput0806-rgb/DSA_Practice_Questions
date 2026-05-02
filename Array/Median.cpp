afeb8da13b58fa8d83f8f3579f85d0d822bfd87d
Shweta Rajput <shwetarajput0806@gmail.com>
2026-05-02 16:41:24 +0530

    Add Median function for two sorted arrays

---
+++ Array/Median.cpp
@@ -0,0 +1,22 @@
+//Median of two sorted array
+#includE<iostream>
+using namespace std;
+int Median(int nums1,int num2,int m , int n){
+    int ans =[]
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            int ans = arr[i]+arr[j];
+        }
+    }
+
+}
+int main(){
+    int nums1 =[1,3];
+    int nums2 =[2];
+    int m =2;
+    int n=1;
+    Median(nums1,nums1,2,1);
+    cout<<"The merge array is";
+
+
+}
