75b9191c320043cd5fc39962bc6577f34bc434ad
Shweta Rajput <shwetarajput0806@gmail.com>
2026-06-02 09:29:39 +0530

    Create ShuffleTheArray.cpp

---
+++ LinearSearch/ShuffleTheArray.cpp
@@ -0,0 +1,20 @@
+//Shuffle the array
+
+
+
+#include<iostream>
+using namespace std;
+int Shuffle(int arr[],int n){
+    int x=0;
+    int y=0;
+    for(int i=0;i<n;i+2){
+        arr[i]=x;
+        cout<<arr[i]<<" "<<endl;
+    }
+}
+
+for(int i=0;i<n)
+int main(){
+    int arr[]={2,,5,1,3,4,7};
+
+}
