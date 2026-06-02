d145ee26a02278d6d87d6befe0b8eb9a36be0560
Shweta Rajput <shwetarajput0806@gmail.com>
2026-06-02 09:26:21 +0530

    Create DisappearningElements.cpp

---
+++ LinearSearch/DisappearningElements.cpp
@@ -0,0 +1,12 @@
+//Find all the disappearning elements int he array
+#include<iostream>
+using namespace std;
+int DisApp(int arr[],int n){
+    for(int num =1;num<=n;num++)
+    for(int i=0;i<n;i++){
+        if(arr[i]==)
+    }
+}
+int main(){
+    int arr[]={1,2,3,6,7,8};
+}
