83c81dc84b0d874b3a2adcdc388e3195b9a2af6a
Shweta Rajput <shwetarajput0806@gmail.com>
2026-07-01 19:21:37 +0530

    Create LargestRectangularArea.cpp

---
+++ Stack/LargestRectangularArea.cpp
@@ -0,0 +1,70 @@
+//largest Rectangular area in histogram
+
+
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<climits>
+using namespace std;
+
+class Solution {
+    private:
+      vector<int> nextSmallerElement(vector<int> arr, int n){
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+
+        for(int i=n-1; i>=0; i--){
+            int curr = arr[i];
+            while(s.top() != -1 && arr[s.top()] >= curr){
+                s.pop();
+            }
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+    vector<int> previousSmallerElement(vector<int> arr, int n){
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+
+        for(int i=0; i<n; i++){
+            int curr = arr[i];
+            while(s.top() != -1 && arr[s.top()] >= curr){
+                s.pop();
+            }
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+    public:
+    int LargestArea(vector<int> &heights){
+        int n = heights.size();
+
+        vector<int> next(n);
+        next = nextSmallerElement(heights, n);
+
+        vector<int> prev(n);
+        prev = previousSmallerElement(heights, n);
+        int area = INT_MIN;
+        for(int i=0; i<n; i++){
+            int l = heights[i];
+            if(next[i] == -1){
+                next[i] = n;
+            }
+            int b = next[i] - prev[i] - 1;
+            int newArea = l*b;
+            area = max(area, newArea);
+        }
+        return area;
+    }
+};
+int main(){
+    vector<int> heights = {2,1,5,6,2,3};
+    Solution obj;
+    int ans = obj.LargestArea(heights);
+    cout<<"Largest Area in Histogram: "<<ans<<endl;
+    return 0;
+}
